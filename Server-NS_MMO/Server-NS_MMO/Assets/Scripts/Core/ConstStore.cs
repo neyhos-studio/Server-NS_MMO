ded29378f4fde29dbab0a2785af0e9a977e04ad1
Amaury NEYHOUSER <44467304+Amaury-NEYHOUSER@users.noreply.github.com>
2019-09-06 07:16:49 +0200

    first commit

---
+++ Server-NS_MMO/Server-NS_MMO/Assets/Scripts/Core/ConstStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Core
+{
+    class ConstStore
+    {
+        public static IPAddress SERVER_ADDRESS = IPAddress.Parse("127.0.0.1");
+        public static int SERVER_PORT = 8888;
+        public static int SERVER_TIMEOUT = 10_000;
+    }
+}
